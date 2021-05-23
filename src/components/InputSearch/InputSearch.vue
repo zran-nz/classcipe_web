b7d817d63349c38e36baf304dbe12db5199be496
yangxunwu <yangxunwu@outlook.com>
2021-05-23 20:31:19 +0800

    add input search component

---
+++ src/components/InputSearch/InputSearch.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="input-search">
+    <a-input v-model="value" @keydown="handleSearch" @focus="handleSearch" class="input-search-item"/>
+    <div class="search-list-wrapper" v-if="showSearchListFlag">
+      <ul class="search-list" >
+        <li class="search-list-item input-search-item" v-for="(item,key) in searchList" :key="key" @click="handleSelectItem(item)">{{ item[label] }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as logger from '@/utils/logger'
+
+export default {
+  name: 'InputSearch',
+  props: {
+    defaultValue: {
+      type: String,
+      default: null
+    },
+    label: {
+      type: String,
+      default: 'name'
+    },
+    searchList: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    showSearchListFlag () {
+      return this.searchList.length > 0
+    }
+  },
+  mounted () {
+    this.globalClick(this.handleClick)
+  },
+  data () {
+    return {
+      value: null
+    }
+  },
+  created () {
+    this.value = this.defaultValue
+  },
+  methods: {
+    handleClick (event) {
+      if (!event.target.classList.contains('input-search-item')) {
+        this.$emit('reset')
+      }
+    },
+    handleSearch () {
+      logger.info('handleSearch' + this.value)
+      this.$emit('search', this.value)
+    },
+    handleSelectItem (item) {
+      logger.info('handleSelectItem', item)
+      this.value = item[this.label]
+      this.$emit('selectItem', item)
+      this.$emit('reset')
+    },
+    handleResetSearchList () {
+      logger.info('handleResetSearchList')
+      this.$emit('reset')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import "~@/components/index.less";
+
+.input-search {
+  position: relative;
+  .search-list-wrapper {
+    position: absolute;
+    top: 45px;
+    z-index: 1000;
+    width: 100%;
+
+    ul {
+      box-shadow: @box-shadow-base;
+      font-variant: initial;
+      background: @input-disabled-bg;
+      border-radius: @border-radius-base;
+      outline: none;
+      margin: 0;
+      padding: 0;
+      list-style: none;
+
+      li {
+        transition: all 0.2s ease-in;
+        cursor: pointer;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        background-color: @select-dropdown-bg;
+        font-size: @select-dropdown-font-size;
+        line-height: @input-height-base - 8px - 2px;
+        padding: @select-dropdown-vertical-padding;
+
+        &:hover,
+        &:focus,
+        &:active {
+          color:  @select-item-selected-color;
+          background-color: @select-item-active-bg;
+          border-color: @select-border-color;
+        }
+
+        &.selected-item {
+          color:  @select-item-selected-color;
+          font-weight: @select-item-selected-font-weight;
+          background-color: @select-item-selected-bg;
+        }
+      }
+
+    }
+  }
+}
+</style>
