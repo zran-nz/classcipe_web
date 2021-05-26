468872d2e3b8e894bea7e91f3c5be4c01d0ed8d9
yangxunwu <yangxunwu@outlook.com>
2021-05-26 20:34:47 +0800

    add unit plan redirect page

---
+++ src/views/teacher/UnitPlanRedirect.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-card :bordered="false" :bodyStyle="{ padding: '16px 24px', height: '100%', minHeight: '500px' }">
+    <a-spin class="redirect-spin" tip="Loading..." size="large"/>
+  </a-card>
+</template>
+
+<script>
+import { PageHeaderWrapper } from '@ant-design-vue/pro-layout'
+import { UnitPlanAddOrUpdate } from '@/api/unitPlan'
+import * as logger from '@/utils/logger'
+
+export default {
+  name: 'UnitPlanRedirect',
+  components: {
+    PageHeaderWrapper
+  },
+  props: {
+    unitPlanId: null
+  },
+  created () {
+    if (this.unitPlanId !== 'create') {
+      this.$router.replace('/teacher/unit-plan/' + this.unitPlanId)
+    } else {
+      const unitPlanData = {
+        name: 'Unnamed Unit Plan'
+      }
+
+      UnitPlanAddOrUpdate(unitPlanData).then((response) => {
+        logger.info('creatingUnitPlan response', response.result)
+        this.$router.replace({
+          path: '/teacher/unit-plan/' + response.result.id
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.redirect-spin {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  margin-left: -20px;
+}
+</style>
