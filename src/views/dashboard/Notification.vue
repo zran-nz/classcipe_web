3c4d4c609e3e3e1fa609f27f2c63d33d6e058c14
grady zhang <grady.zhang@zoom.us>
2021-10-16 18:23:43 +0800

    add notication

--- src/views/dashboard/Notification.vue
+++ src/views/dashboard/Notification.vue
@@ -1,6 +1,22 @@
 <template>
-  <a-card :bordered="false">
-    Notification
+  <a-card :bordered="false" title="Notification">
+    <div>
+      <a-list item-layout="vertical" size="large" :data-source="data">
+        <a-list-item slot="renderItem" key="item.title" slot-scope="item">
+          <!--          <img-->
+          <!--            slot="extra"-->
+          <!--            width="272"-->
+          <!--            alt="logo"-->
+          <!--            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"-->
+          <!--          />-->
+          <a-list-item-meta :description="item.description">
+            <a slot="title" :href="item.href">{{ item.title }}</a>
+            <a-avatar slot="avatar" :src="item.avatar" />
+          </a-list-item-meta>
+          <a-button type="primary" @click="handleOk">立即查看</a-button>
+        </a-list-item>
+      </a-list>
+    </div>
   </a-card>
 </template>
 
@@ -18,17 +34,22 @@ export default {
   data () {
     this.directionType = directionType
     return {
-      // title: this.$t('result.success.title'),
-      // description: this.$t('result.success.description')
+      data: [
+        {
+          'title': 'Mike invite you to collabrate the task ：History of China',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ',
+          'time': '2021 09 13  15:00',
+          'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png'
+      }, {
+          'title': 'Mike invite you to collabrate the task ：History of China',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ',
+          'time': '2021 09 13  15:00',
+          'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png'
+        }]
     }
   },
   computed: {
-    result () {
-      return {
-      title: this.$t('result.success.title'),
-      description: this.$t('result.success.description')
-      }
-    }
+
   }
 }
 </script>
