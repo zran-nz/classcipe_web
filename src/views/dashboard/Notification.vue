a65e9c8bac4a5e9681dd2ac4302b85367e2970c5
grady zhang <grady.zhang@zoom.us>
2021-10-16 22:55:21 +0800

    change notification

--- src/views/dashboard/Notification.vue
+++ src/views/dashboard/Notification.vue
@@ -1,19 +1,24 @@
 <template>
   <a-card :bordered="false" title="Notification">
+    <a slot="extra" href="#">Mark as read</a>
     <div>
-      <a-list item-layout="vertical" size="large" :data-source="data">
-        <a-list-item slot="renderItem" key="item.title" slot-scope="item">
-          <!--          <img-->
-          <!--            slot="extra"-->
-          <!--            width="272"-->
-          <!--            alt="logo"-->
-          <!--            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"-->
-          <!--          />-->
-          <a-list-item-meta :description="item.description">
-            <a slot="title" :href="item.href">{{ item.title }}</a>
-            <a-avatar slot="avatar" :src="item.avatar" />
+      <a-list
+        item-layout="vertical"
+        size="large"
+        :loading="loading"
+        :pagination="pagination"
+        :data-source="messageList">
+        <a-list-item
+          slot="renderItem"
+          key="item.id"
+          slot-scope="item"
+          :class="{'my-list-item': true}">
+          <div slot="extra" >{{ item.sendTime| dayjs }}</div>
+          <a-list-item-meta :description="item.msgContent">
+            <a slot="title" :href="item.href">{{ item.titile }}</a>
+            <img class="message-icon" slot="avatar" src="~@/assets/icons/header/message.png"/>
           </a-list-item-meta>
-          <a-button type="primary" @click="handleOk">立即查看</a-button>
+          <a-button type="primary" @click="handleView(item)">View details</a-button>
         </a-list-item>
       </a-list>
     </div>
@@ -22,6 +27,8 @@
 
 <script>
 import { baseMixin } from '@/store/app-mixin'
+import * as logger from '@/utils/logger'
+import { NoticeQueryById, ListByMessage } from '@/api/notice'
 
 const directionType = {
   horizontal: 'horizontal',
@@ -34,22 +41,78 @@ export default {
   data () {
     this.directionType = directionType
     return {
-      data: [
-        {
-          'title': 'Mike invite you to collabrate the task ：History of China',
-          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ',
-          'time': '2021 09 13  15:00',
-          'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png'
-      }, {
-          'title': 'Mike invite you to collabrate the task ：History of China',
-          'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ',
-          'time': '2021 09 13  15:00',
-          'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png'
-        }]
+      pagination: {
+        onChange: page => {
+          logger.info('pagination onChange', page)
+          this.pageNo = page
+          this.loadMessageData()
+        },
+        showTotal: total => `Total ${total} items`,
+        total: 0,
+        pageSize: 15
+      },
+      pageNo: 1,
+      loading: true,
+      messageList: []
     }
   },
   computed: {
 
+  },
+  created () {
+    this.loadMessageData()
+  },
+  methods: {
+    loadMessageData () {
+      this.loading = true
+      ListByMessage({
+        pageNo: this.pageNo,
+        pageSize: this.pagination.pageSize
+      }).then((res) => {
+        logger.info('ListByMessage ', res)
+        this.loading = false
+        if (res.success) {
+          if (res.result.records && res.result.records.length) {
+            this.messageList = res.result.records
+            this.pagination.total = res.result.total
+            this.pagination.current = res.result.current
+          } else {
+            this.myContentList = []
+            this.pagination.total = 0
+          }
+        } else {
+          this.$message.error(res.message)
+        }
+      })
+    },
+    handleView (key, data) {
+      this.$notification.close(key)
+      var id = data.msgId
+      NoticeQueryById({ id: id }).then((res) => {
+        if (res.success) {
+          var record = res.result
+          this.showAnnouncement(record)
+        }
+      })
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+@import "~@/components/index.less";
+.message-icon{
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 50%;
+}
+.no-data {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 60%;
+  margin: auto;
+}
+</style>
