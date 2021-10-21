a4844298c229eeebbd5821e5534f32578de22a94
grady zhang <grady.zhang@zoom.us>
2021-10-21 23:11:49 +0800

    fix redicr bug

--- src/views/addon/PageRedirect.vue
+++ src/views/addon/PageRedirect.vue
@@ -4,20 +4,20 @@
 </template>
 
 <script>
-  import storage from 'store'
-  import { ACCESS_TOKEN } from '@/store/mutation-types'
-  import { typeMap } from '@/const/teacher'
-  import { SESSION_ACTIVE_KEY } from '@/const/common'
+
+import storage from 'store'
+import { ACCESS_TOKEN } from '@/store/mutation-types'
+import { SESSION_ACTIVE_KEY } from '@/const/common'
 
   export default {
   name: 'PageRedirect',
   components: {
   },
-    data () {
-      return {
-        type: 4
-      }
-    },
+  data () {
+    return {
+      type: 4
+    }
+  },
   created () {
     let token = this.$route.query.token
     if (!token) {
@@ -26,10 +26,21 @@
     if (token) {
       storage.set(ACCESS_TOKEN, token)
       window.sessionStorage.setItem(SESSION_ACTIVE_KEY, token)
-      this.$router.push('/teacher/add-task/' + this.id)
-    } else {
-      this.$router.push({ path: '/user/login' })
     }
+    this.$logger.info('AuthCheck loaded!')
+    // 没有session 数据，尝试验证token，如果失效那么重新登录
+    this.$store.dispatch('GetInfo').then((data) => {
+      this.$logger.info('AuthCheck token valid, go to path', data)
+      this.$router.push('/teacher/add-task/' + this.id)
+    }).catch(err => {
+      this.$logger.warn('AuthCheck token invalid, try login', err)
+      this.$store.dispatch('ClearAuth').then(() => {
+        this.$router.push({ name: 'login', query: { redirect: this.$route.fullPath } })
+      }).catch(err => {
+        this.$logger.error('AuthCheck logout failed', err)
+        this.goHomeHandle()
+      })
+    })
   },
   props: {
     id: {
