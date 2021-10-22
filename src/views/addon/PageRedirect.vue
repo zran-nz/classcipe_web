2c4f117db5c9eafabb0f1f3ab736709260b232be
grady.zhang <grady.zhang@zoom.us>
2021-10-22 10:11:05 +0800

    merge conflict

--- src/views/addon/PageRedirect.vue
+++ src/views/addon/PageRedirect.vue
@@ -27,20 +27,7 @@ export default {
       storage.set(ACCESS_TOKEN, token)
       window.sessionStorage.setItem(SESSION_ACTIVE_KEY, token)
     }
-    this.$logger.info('AuthCheck loaded!')
-    // 没有session 数据，尝试验证token，如果失效那么重新登录
-    this.$store.dispatch('GetInfo').then((data) => {
-      this.$logger.info('AuthCheck token valid, go to path', data)
-      this.$router.push('/teacher/add-task/' + this.id)
-    }).catch(err => {
-      this.$logger.warn('AuthCheck token invalid, try login', err)
-      this.$store.dispatch('ClearAuth').then(() => {
-        this.$router.push({ name: 'login', query: { redirect: this.$route.fullPath } })
-      }).catch(err => {
-        this.$logger.error('AuthCheck logout failed', err)
-        this.goHomeHandle()
-      })
-    })
+    this.$router.push('/teacher/add-task/' + this.id)
   },
   props: {
     id: {
