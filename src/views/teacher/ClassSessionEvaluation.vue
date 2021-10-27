0b502ec5c1d8312d6c707fd29b5f6897e96c0a86
yangxunwu <yangxunwu@outlook.com>
2021-10-27 18:38:18 +0800

    add group list

--- src/views/teacher/ClassSessionEvaluation.vue
+++ src/views/teacher/ClassSessionEvaluation.vue
@@ -13,7 +13,116 @@
       />
     </div>
     <a-card :bordered="false" :bodyStyle="{ padding: '16px 24px', height: '100%', minHeight: '500px' }" :loading="loading" >
-      {{ form }}
+      <div class="class-session-evaluation">
+        <div class="header">
+          <div class="left-action">
+            <div class="add-btn">
+              <a-button type="primary" @click="handleAddFormTable">
+                <div class="btn-content">
+                  <a-icon type="plus-circle" />
+                  <span class="btn-text">
+                    Add rubric format
+                  </span>
+                </div>
+              </a-button>
+            </div>
+          </div>
+          <div class="right-icon">
+            <div class="icon-type-item">
+              <div class="icon-item">
+                <teacher-icon />
+              </div>
+              <div class="label">Teacher</div>
+            </div>
+            <div class="icon-type-item">
+              <div class="icon-item">
+                <student-icon />
+              </div>
+              <div class="label">Student</div>
+            </div>
+            <div class="icon-type-item">
+              <div class="icon-item">
+                <peer-icon />
+              </div>
+              <div class="label">Peer</div>
+            </div>
+          </div>
+        </div>
+        <div class="form-table-tabs">
+          <div
+            :class="{'form-table-item': true,
+                     'active-table': currentActiveFormTable === tableItem.tableName}"
+            v-for="(tableItem, idx) in formTableList"
+            @click="handleActiveTable(idx, tableItem)"
+            :key="idx">
+            {{ tableItem.tableName }}
+          </div>
+        </div>
+        <div class="body">
+          <div class="class-group">
+            <div class="class-student-wrapper">
+              <div class="info-bar">
+                <div class="info-bar-item">
+                  {{ groupNum }} groups {{ studentNum }} students
+                </div>
+              </div>
+              <div class="group-list-wrapper">
+                <div :class="{'group-item': true, 'selected-group': selectedGroupList.indexOf(group.groupName) !== -1}" v-for="(group, gIdx) in classGroup" :key="gIdx">
+                  <div class="group-item-info" @click="handleSelectGroup(group)">
+                    <div class="group-left">
+                      <div class="group-icon">
+                        <group-icon />
+                      </div>
+                      <div class="group-name">
+                        {{ group.groupName }} ({{ group.groupStudents.length }})
+                      </div>
+                      <div class="group-select-status">
+                        <template v-if="selectedGroupList.indexOf(group.groupName) !== -1">
+                          <a-icon type="check-circle" style="{color: #07AB84}" theme="filled" class="my-selected-icon"/>
+                        </template>
+                      </div>
+                    </div>
+                    <div class="group-right" @click="handleToggleGroupExpand(group, $event)">
+                      <div class="group-expand-status">
+                        <template v-if="group.expand">
+                          <arrow-down />
+                        </template>
+                        <template v-if="!group.expand">
+                          <arrow-top />
+                        </template>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="group-student-list" v-show="group.expand">
+                    <div class="student-list">
+                      <div :class="{'list-item': true, 'selected-student': selectedStudentEmailList.indexOf(student.studentEmail) !== -1}" v-for="(student, sIndex) in group.groupStudents" :key="sIndex" @click="handleClickStudent(student)">
+                        <div class="student-avatar">
+                          <img :src="student.studentAvatar" alt="" v-if="student.studentAvatar" />
+                          <img slot="prefix" src="~@/assets/icons/evaluation/default_avatar.png" alt="" v-if="!student.studentAvatar" />
+                        </div>
+                        <div class="student-name" :data-email="student.studentEmail">
+                          {{ student.studentName }}
+                        </div>
+                        <div class="select-status-icon" v-if="(selectedStudentEmailList.indexOf(student.studentEmail) !== -1) || selectedGroupList.indexOf(group.groupName) !== -1">
+                          <a-icon type="check-circle" style="{color: #07AB84}" theme="filled" class="my-selected-icon"/>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="form-table-content">
+            <div class="table-content">
+              <rubric-one
+                ref="rubric"
+                mode="evaluate"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </a-card>
   </div>
 </template>
@@ -23,12 +132,26 @@
 import { formatLocalUTC } from '@/utils/util'
 import RubricOne from '@/components/Evaluation/RubricOne'
 import CommonFormHeader from '@/components/Common/CommonFormHeader'
+import GrayIcon from '@/assets/svgIcon/evaluation/GrayIcon.svg?inline'
+import PeerIcon from '@/assets/svgIcon/evaluation/PeerIcon.svg?inline'
+import StudentIcon from '@/assets/svgIcon/evaluation/StudentIcon.svg?inline'
+import TeacherIcon from '@/assets/svgIcon/evaluation/TeacherIcon.svg?inline'
+import GroupIcon from '@/assets/svgIcon/evaluation/qunzu.svg?inline'
+import ArrowDown from '@/assets/svgIcon/evaluation/arrow_down.svg?inline'
+import ArrowTop from '@/assets/svgIcon/evaluation/arrow_top.svg?inline'
 
 export default {
   name: 'ClassSessionEvaluation',
   components: {
     RubricOne,
-    CommonFormHeader
+    CommonFormHeader,
+    GrayIcon,
+    PeerIcon,
+    StudentIcon,
+    TeacherIcon,
+    GroupIcon,
+    ArrowDown,
+    ArrowTop
   },
   props: {
     taskId: {
@@ -54,29 +177,87 @@ export default {
     return {
       loading: true,
       isExistFormTable: false, // 是否已经添加过表格
-
+      currentActiveFormTable: '21 century skill',
       form: { // 基础表单数据
         name: 'Untitled Evaluation',
+        comment: null,
         updateTime: '2021-10-28 12:43:12',
         className: 'class6',
         type: 7, // TODO Evaluation 新增表单类型 classSessionEvaluation:7
         createBy: 'yangxunwu@gmail.com'
       },
-      formTableData: [], // 评估表格数据
+      formTableList: [
+        {
+          tableName: '21 century skill',
+          tableData: []
+        },
+        {
+          tableName: 'Form',
+          tableData: []
+        }
+      ], // 评估表格数据
       classGroup: [
         {
           groupId: 1,
           groupName: '小组1',
           selectedStatus: false,
+          expand: false,
           groupStudents: [
             {
-              studentName: 'xunwu',
+              studentName: 'xunwu1',
+              studentEmail: 'xunwu1',
+              studentAvatar: '',
+              alreadyEvaluatedFormTable: [] // 已经评估过了的表格的
+            },
+            {
+              studentName: 'xunwu2',
+              studentEmail: 'xunwu2',
+              studentAvatar: '',
+              alreadyEvaluatedFormTable: [] // 已经评估过了的表格的
+            },
+            {
+              studentName: 'xunwu3',
+              studentEmail: 'xunwu3',
+              studentAvatar: '',
+              alreadyEvaluatedFormTable: [] // 已经评估过了的表格的
+            },
+            {
+              studentName: 'xunwu4',
+              studentEmail: 'xunwu4',
+              studentAvatar: '',
+              alreadyEvaluatedFormTable: [] // 已经评估过了的表格的
+            },
+            {
+              studentName: 'xunwu5',
+              studentEmail: 'xunwu5',
+              studentAvatar: '',
+              alreadyEvaluatedFormTable: [] // 已经评估过了的表格的
+            }
+          ]
+        },
+        {
+          groupId: 2,
+          groupName: '小组2',
+          selectedStatus: false,
+          expand: false,
+          groupStudents: [
+            {
+              studentName: 'xiaoming',
+              studentEmail: 'xiaoming',
               studentAvatar: '',
               alreadyEvaluatedFormTable: [] // 已经评估过了的表格的
             }
           ]
         }
-      ] // 班级分组信息
+      ], // 班级分组信息
+
+      selectedGroupList: [],
+      studentList: [],
+      selectedStudentEmailList: [],
+      selectedStudentNameList: [],
+
+      groupNum: 3,
+      studentNum: 20
     }
   },
   created () {
@@ -89,6 +270,43 @@ export default {
       // TODO Evaluation 根据当前的taskId、班级classId查询对应的评估数据：班级分组、评估表单数据、自评他评数据
       this.loading = false
     },
+
+    handleActiveTable (idx, tableItem) {
+      this.$logger.info('handleActiveTable ' + idx, tableItem)
+      if (this.currentActiveFormTable !== tableItem.tableName) {
+        this.currentActiveFormTable = tableItem.tableName
+      }
+    },
+    handleClickStudent (student) {
+      this.$logger.info('handleClickStudent', student)
+      const index = this.selectedStudentEmailList.indexOf(student.studentEmail)
+      if (index === -1) {
+        this.selectedStudentEmailList.push(student.studentEmail)
+      } else {
+        this.selectedStudentEmailList.splice(index, 1)
+      }
+    },
+
+    handleSelectGroup (group) {
+      this.$logger.info('handleSelectGroup', group)
+      const index = this.selectedGroupList.indexOf(group.groupName)
+      if (index === -1) {
+        this.selectedGroupList.push(group.groupName)
+      } else {
+        this.selectedGroupList.splice(index, 1)
+      }
+    },
+
+    handleToggleGroupExpand (group, event) {
+      event.stopPropagation()
+      event.preventDefault()
+      this.$logger.info('handleToggleGroupExpand', group)
+      group.expand = !group.expand
+    },
+
+    handleAddFormTable () {
+    },
+
     goBack () {
       if (window.history.length <= 1) {
         this.$router.push({ path: '/teacher/main/created-by-me' })
@@ -115,5 +333,219 @@ export default {
 
 <style lang="less" scoped>
 @import "~@/components/index.less";
+
+.class-session-evaluation {
+  .header {
+    display: flex;
+    justify-content: space-between;
+    .right-icon {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      .icon-type-item {
+        user-select: none;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        margin-left: 20px;
+        .label {
+          padding: 0 5px;
+          font-size: 14px;
+          font-family: Arial;
+          font-weight: 400;
+          line-height: 0px;
+          color: #070707;
+        }
+      }
+    }
+  }
+
+  .form-table-tabs {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    border-bottom: 1px solid #E7E7E7;
+    margin-top: 10px;
+    .form-table-item {
+      padding: 5px 15px;
+      cursor: pointer;
+      font-family: Arial;
+      font-weight: 400;
+      line-height: 20px;
+      text-align: center;
+      color: #070707;
+      border-bottom: 3px solid #fff;
+    }
+    .active-table {
+      border-bottom: 3px solid #07AB84 !important;
+    }
+  }
+
+  .body {
+    padding-top: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+
+    .class-group {
+      width: 250px;
+      .class-student-wrapper {
+        height: 539px;
+        background: #FFFFFF;
+        border: 1px solid #dadada;
+        opacity: 1;
+        border-radius: 0px 0px 4px 4px;
+        .info-bar {
+          margin-bottom: 3px;
+          .info-bar-item {
+            padding: 10px 0 10px 0;
+            cursor: pointer;
+            font-family: Inter-Bold;
+            font-size: 12px;
+            color: #999;
+            text-align: center;
+          }
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+        }
+
+        .group-list-wrapper {
+          height: 503px;
+          display: flex;
+          flex-direction: column;
+          max-height: 500px;
+          overflow-y: scroll;
+          &::-webkit-scrollbar {
+            width: 5px;
+            height: 5px;
+          }
+          &::-webkit-scrollbar-track {
+            border-radius: 3px;
+            background: rgba(0,0,0,0.00);
+            -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.01);
+          }
+          /* 滚动条滑块 */
+          &::-webkit-scrollbar-thumb {
+            border-radius: 5px;
+            background: rgba(0,0,0,0.12);
+            -webkit-box-shadow: inset 0 0 10px rgba(0,0,0,0.01);
+          }
+          .group-item {
+            display: flex;
+            flex-direction: column;
+            .group-item-info {
+              line-height: 30px;
+              display: flex;
+              user-select: none;
+              flex-direction: row;
+              align-items: center;
+              justify-content: space-between;
+              padding: 13px 15px;
+              cursor: pointer;
+
+              &:hover {
+                background-color: #F7F8FF;
+              }
+
+              .group-left {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                position: relative;
+
+                .group-icon {
+                  display: flex;
+                  flex-direction: row;
+                  align-items: center;
+                  svg {
+                    height: 20px;
+                  }
+                }
+
+                .group-name {
+                  padding: 0 5px;
+                }
+
+                .group-select-status {
+                  margin-left: 5px;
+                  width: 30px;
+                  user-select: none;
+                  svg {
+                    fill: #07AB84;
+                    color: #07AB84;
+                  }
+                }
+              }
+
+              .group-right {
+              }
+            }
+
+            .group-student-list {
+              .student-list {
+                display: flex;
+                flex-direction: column;
+                .list-item {
+                  cursor: pointer;
+                  padding: 13px 30px 13px 15px;
+                  user-select: none;
+                  display: flex;
+                  flex-direction: row;
+                  align-items: center;
+                  justify-content: flex-start;
+                  position: relative;
+
+                  &:hover {
+                    background-color: #F7F8FF;
+                  }
+
+                  .student-avatar {
+                    img {
+                      width: 30px;
+                    }
+                  }
+
+                  .student-name {
+                    padding: 0 5px;
+                    font-family: Inter-Bold;
+                    line-height: 24px;
+                    color: #11142D;
+                  }
+                  .select-status-icon {
+                    position: absolute;
+                    right: 15px;
+                    top: 50%;
+                    margin-top: -7.5px;
+                    img {
+                      height: 15px;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .form-table-content {
+      width: calc(100% - 250px);
+    }
+  }
+}
+
+.my-selected-icon {
+  fill: #07AB84;
+  color: #07AB84;
+}
+
+.selected-student {
+  background-color: rgba(21, 195, 154, 0.15);
+
+  &:hover {
+    background-color: rgba(21, 195, 154, 0.15) !important;
+  }
+}
 
 </style>
