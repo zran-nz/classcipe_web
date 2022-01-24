c0ae9aff04b289854a82d09f576b8262f1883282
yangxunwu <yangxunwu@outlook.com>
2022-01-24 16:02:17 +0800

    fix new browser bug

---
+++ src/api/schoolClass.js
@@ -0,0 +1,7 @@
+import request from '@/utils/request'
+
+export const schoolClassAPIUrl = {
+  SchoolClassGetMyClasses: '/classcipe/api/school/class/getMyClasses',
+  SchoolClassClassList: '/classcipe/api/school/class/list',
+  SchoolClassListClassMembers: '/classcipe/api/school/class/listClassMembers'
+}
