bbf9b66e008938a48af16541b1fe96e09334f200
Acan <abans@qq.com>
2022-02-14 09:12:10 +0800

    disableHostCheck

--- vue.config.js
+++ vue.config.js
@@ -110,6 +110,7 @@ const vueConfig = {
   devServer: {
     // development server port 8000
     port: 8000,
+    disableHostCheck: true,
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     proxy: {
       '/classcipe': {
