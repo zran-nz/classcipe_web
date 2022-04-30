c1f6b2d07b380ba33a920040286bf7ebbadf7724
yangxunwu <yangxunwu@gmail.com>
2022-05-01 07:31:29 +0800

    添加assessment tool

---
+++ src/components/AssessmentTool/Rubric.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='rubric'></div>
+</template>
+
+<script>
+export default {
+  name: 'Rubric',
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+@import "~@/components/index.less";
+
+</style>
