c82f0d0bb30114b1ebeefb125560ce8761a52f89
yangxunwu <yangxunwu@gmail.com>
2022-05-07 18:49:09 +0800

    添加自定义搜索输入按钮

---
+++ src/components/Common/CustomSearchInput.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class='custom-search-input'>
+    <a-input v-model='data' :placeholder='placeholder' @keyup.native.enter='handleSearch' @keydown.native='handleUpdateValue'/>
+    <div class='search-icon' @click='handleSearch'>
+      <search-icon />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import SearchIcon from '@/assets/v2/icons/search.svg?inline'
+
+export default {
+  name: 'CustomSearchInput',
+  components: {
+    SearchIcon
+  },
+  props: {
+    value: {
+      type: String,
+      default: null
+    },
+    placeholder: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      data: null
+    }
+  },
+  created() {
+    this.data = this.value
+  },
+  methods: {
+    handleSearch () {
+      this.$logger.info('search', this.data)
+      this.$emit('search', this.data)
+    },
+    handleUpdateValue () {
+      this.$emit('update:value', this.data)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+@import "~@/components/index.less";
+
+.custom-search-input {
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  cursor: pointer;
+
+  .search-icon {
+    position: absolute;
+    width: 30px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: flex-end;
+    padding-right: 5px;
+    cursor: pointer;
+
+    svg {
+      width: 13px;
+      height: 13px;
+    }
+  }
+
+  /deep/ input {
+    padding-left: 30px;
+    border-color: #EEF1F6;
+    border-width: 2px !important;
+    border-radius: 6px;
+    transition: all 0.3s ease-in-out;
+
+    &:focus {
+      box-shadow: none;
+      border-color: #d4d9e0;
+    }
+
+    &::placeholder {
+      font-family: Arial;
+      font-weight: 400;
+      color: #44464F;
+      font-size: 13px;
+    }
+  }
+}
+</style>
