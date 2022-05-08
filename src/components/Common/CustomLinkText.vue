d42fd0fb35f02573f8f7e84c890d7ccb6bde7950
yangxunwu <yangxunwu@gmail.com>
2022-05-08 18:27:55 +0800

    自定义链接文本组件

---
+++ src/components/Common/CustomLinkText.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class='link-text' :style="{'font-size': size + 'px', 'line-height': size + 'px'}" @click='handleClick'>
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomLinkText',
+  props: {
+    text: {
+      type: String,
+      default: null
+    },
+    size: {
+      type: Number,
+      default: 14
+    }
+  },
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+@import "~@/components/index.less";
+.link-text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Arial;
+  color: #8A8D93;
+  font-weight: normal;
+
+  &:hover {
+    text-decoration: underline;
+    color: #007A8E;
+  }
+}
+
+</style>
