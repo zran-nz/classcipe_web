c34d3ccfc71ad06deaab0491f77166eb16d84a4d
yangxunwu <yangxunwu@gmail.com>
2022-05-08 09:41:56 +0800

    新增表单页面元素、选择组件

---
+++ src/components/Common/CustomFormItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class='cc-custom-form-item'>
+    <div class='label'>
+      <slot name='label'></slot>
+      <div class='tips'>
+        <slot name='tips'></slot>
+      </div>
+    </div>
+    <div class='form-item-content'>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomFormItem',
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+@import "~@/components/index.less";
+
+.cc-custom-form-item {
+  padding-bottom: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  overflow: visible;
+  .label {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    font-size: 14px;
+    font-family: Arial;
+    font-weight: bold;
+    color: #202020;
+    line-height: 40px;
+
+    .tips {
+      padding-left: 5px;
+    }
+  }
+
+  .form-item-content {
+    box-sizing: border-box;
+    overflow: visible;
+  }
+}
+</style>
---
+++ src/components/Common/CustomRadioButtonGroup.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class='custom-radio-button-group'>
+    <a-space :size="20">
+      <a-button
+        class='radio-button-item'
+        :class="{'checked-item': checkedItem && checkedItem.value === item.value }"
+        v-for='(item, idx) in list'
+        :key='idx'
+        @click='handleClick(item)'>
+        {{ item[displayProperty] }}
+      </a-button>
+    </a-space>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomRadioButtonGroup',
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    },
+    displayProperty: {
+      type: String,
+      default: 'name'
+    },
+    value: {
+      type: null,
+      default: null
+    }
+  },
+  data() {
+    return {
+      checkedItem: null
+    }
+  },
+  created() {
+    if (this.value) {
+      this.checkedItem = this.list.find(item => item.value === this.value)
+    }
+  },
+  methods: {
+    handleClick (item) {
+      this.$logger.info('handleClick', item)
+      this.checkedItem = item
+      this.$emit('change', item)
+      this.$emit('update:value', item.value)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+@import "~@/components/index.less";
+
+.custom-radio-button-group {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  .radio-button-item {
+    min-width: 150px;
+    padding: 5px 0;
+    height: auto;
+    border-radius: 5px;
+    font-family: Arial;
+    font-weight: 400;
+    color: #131824;
+    background: #FFFFFF;
+    border: 1px solid #E1E6ED;
+  }
+
+  .checked-item {
+    background: #F8FDFC;
+    border: 1px solid #20B890;
+    border-radius: 8px;
+  }
+}
+</style>
