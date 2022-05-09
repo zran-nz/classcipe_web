3e9645af9f5060f0a01abd11d1b023dbb635f2cf
yangxunwu <yangxunwu@gmail.com>
2022-05-09 15:46:37 +0800

    添加link category api

---
+++ src/api/v2/mycontent.js
@@ -0,0 +1,52 @@
+import request from '@/utils/request'
+
+export const MyContentV2Url = {
+  GetLinkCategory: '/classcipe/api/v2/mycontent/getLinkCategory',
+  AddOrUpdateLinkCategory: '/classcipe/api/v2/mycontent/addOrUpdateLinkCategory',
+  DeleteLinkCategory: '/classcipe/api/v2/mycontent/deleteLinkCategory'
+}
+
+/**
+ * @param parameter
+ * @returns {AxiosPromise}
+ * @constructor
+ */
+export function GetLinkCategory (parameter) {
+  return request({
+    url: MyContentV2Url.GetLinkCategory,
+    method: 'get',
+    params: parameter
+  })
+}
+
+/**
+ * @param parameter
+ * @returns {AxiosPromise}
+ * @constructor
+ */
+export function AddOrUpdateLinkCategory (parameter) {
+  return request({
+    url: MyContentV2Url.AddOrUpdateLinkCategory,
+    method: 'post',
+    data: parameter,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+}
+
+/**
+ * @param parameter
+ * @returns {AxiosPromise}
+ * @constructor
+ */
+export function DeleteLinkCategory (parameter) {
+  return request({
+    url: MyContentV2Url.DeleteLinkCategory,
+    method: 'post',
+    data: parameter,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+}
