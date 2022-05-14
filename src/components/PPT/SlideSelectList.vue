47de83550ba0a2e1ed224fba6cc6248eadbc5f10
yangxunwu <yangxunwu@gmail.com>
2022-05-14 19:48:42 +0800

    PPT选择浏览列表

---
+++ src/components/PPT/SlideSelectList.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class='slide-select-list'>
+    <div class='slide-source-switch'>
+      <a-radio-group default-value="Recommend" button-style="solid" class='cc-radio-group' v-model='filterSourceType' @change='handleSearchByFilter'>
+        <a-radio-button :value="sourceType.Recommend">
+          Recommend
+        </a-radio-button>
+        <a-radio-button :value="sourceType.SlideTemplate">
+          Slide template
+        </a-radio-button>
+        <a-radio-button :value="sourceType.MyContent">
+          My content
+        </a-radio-button>
+      </a-radio-group>
+    </div>
+    <div class='search-bar'>
+      <content-filter @search='handleSearchByInputFilter'/>
+    </div>
+    <div class='slide-list'>
+      <template v-if='searching'>
+        <a-skeleton />
+      </template>
+      <template v-else>
+        <template v-if='slideList.length'>
+          <div class='slide-item' v-for='slide in slideList' :key='slide.id'>
+            <slide-viewer
+              :title='slide.name'
+              :show-title='true'
+              :img-list='slide.images'
+              :width="'300px'"
+              :height="'180px'"/>
+          </div>
+        </template>
+        <template v-if='slideList.length === 0'>
+          <common-no-data />
+        </template>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { FilterTemplates, recommendTemplates } from '@/api/template'
+import CustomSearchInput from '@/components/Common/CustomSearchInput'
+import ContentFilter from '@/components/MyContentV2/ContentFilter'
+import CommonNoData from '@/components/Common/CommonNoData'
+import SlideViewer from '@/components/PPT/SlideViewer'
+
+const sourceType = {
+  Recommend: 1,
+  SlideTemplate: 2,
+  MyContent: 3
+}
+
+export default {
+  name: 'SlideSelectList',
+  components: { SlideViewer, CommonNoData, ContentFilter, CustomSearchInput },
+  props: {
+    sourceId: {
+      type: String,
+      default: '1522782224384061441' // TODO 删除mock id
+    }
+  },
+  data() {
+    return {
+      filterSourceType: sourceType.Recommend,
+      sourceType: sourceType,
+      slideList: [],
+      searching: true
+    }
+  },
+  created() {
+    this.handleSearchSlide()
+  },
+  methods: {
+    handleSearchByFilter (data) {
+      this.handleSearchSlide()
+    },
+
+    handleSearchByInputFilter (data) {
+      this.handleSearchSlide()
+    },
+
+    handleSearchSlide () {
+      switch (this.filterSourceType) {
+        case sourceType.Recommend:
+          this.getRecommendSlide()
+          break
+        case sourceType.SlideTemplate:
+          this.getTemplateSlide()
+          break
+        case sourceType.MyContent:
+          this.getMyContentSlide()
+          break
+        default:
+          break
+      }
+    },
+
+    getRecommendSlide () {
+      this.searching = true
+      recommendTemplates({
+        taskId: this.sourceId
+      }).then(res => {
+        this.$logger.info('recommendTemplates res', res)
+        if (res && res.result) {
+          this.slideList = res.result
+        }
+      }).finally(() => {
+        this.searching = false
+      })
+    },
+    getTemplateSlide () {
+      this.searching = true
+      FilterTemplates({}).then(res => {
+        this.$logger.info('getTemplateSlide res', res)
+        if (res && res.result) {
+          this.slideList = res.result
+        }
+      }).finally(() => {
+        this.searching = false
+      })
+    },
+    getMyContentSlide () {
+
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+@import "~@/components/index.less";
+
+.search-bar {
+  margin: 10px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.slide-list {
+  height: calc(100vh - 280px);
+  overflow-y: auto;
+  transition: all 0.2s ease-in-out;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+  .slide-item {
+    margin: 0 20px 20px 0;
+    border: 1px solid #E1E1E1;
+    box-shadow: 0 1px 2px 0 rgba(65, 65, 65, 0.07);
+  }
+}
+
+</style>
---
+++ src/components/PPT/SlideViewer.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class='slide-viewer'>
+    <div class='hover-mask'>
+      <div class='hover-action'>
+        <a-button type='primary' shape='round' class='cc-slide-btn' @click='handlePreview'>
+          <a-icon type='eye' /> Preview
+        </a-button>
+        <a-button shape='round' class='cc-slide-btn'  @click='handleAdd'>
+          <a-icon type='plus-circle' /> Add
+        </a-button>
+      </div>
+    </div>
+    <a-carousel :style="{width: width, height: height}">
+      <div class='slider-img-cover' :style="{width: width, height: height, backgroundImage: 'url(' + image + ')'}" v-for='(image,idx) in imgList' :key='idx'>
+      </div>
+    </a-carousel>
+    <div class='slide-title' v-show='showTitle' :style="{width: width}">
+      {{ title}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { TemplatesGetPresentation } from '@/api/template'
+export default {
+  name: 'SlideViewer',
+  props: {
+    imgList: {
+      type: Array,
+      required: true
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    slideId: {
+      type: String,
+      default: null
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '100%'
+    },
+    showTitle: {
+      type: Boolean,
+      default: false
+    },
+    showScrollBar: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {
+    handlePreview () {
+      this.$emit('preview', this.slideId)
+    },
+    handleAdd () {
+      this.$emit('add', this.slideId)
+    },
+  }
+}
+</script>
+
+<style lang='less' scoped>
+@import "~@/components/index.less";
+
+.slider-img-cover {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 100%;
+}
+
+.slide-title {
+  text-align: center;
+  font-family: Arial;
+  font-weight: 400;
+  color: #282828;
+  background-color: #ffffff;
+  cursor: pointer;
+  line-height: 30px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  word-break: break-all;
+  padding: 0 15px;
+  box-sizing: border-box;
+}
+
+.slide-viewer {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  .hover-mask {
+    opacity: 0;
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 100;
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: all 0.3s ease-in-out;
+    .hover-action {
+      width: 100px;
+      position: absolute;
+      left: 50%;
+      margin-left: -50px;
+      top: 50%;
+      margin-top: -40px;
+
+    }
+  }
+
+  &:hover {
+    .hover-mask {
+      display: block;
+      opacity: 1;
+    }
+  }
+}
+
+.cc-slide-btn {
+  margin: 5px 0;
+  width: 110px;
+  line-height: 20px;
+}
+</style>
