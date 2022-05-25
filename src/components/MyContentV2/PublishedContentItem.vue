ad05c572c852ac958bd31532be78a6c9cde69cd4
yangxunwu <yangxunwu@gmail.com>
2022-05-25 12:40:32 +0800

    修改published页面

--- src/components/MyContentV2/PublishedContentItem.vue
+++ src/components/MyContentV2/PublishedContentItem.vue
@@ -7,47 +7,86 @@
     <div class='detail'>
       <div class='detail-content' @click='handlePreviewDetail(content)'>
         <div class='base-info'>
-          <div class='header-info'>
-            <div class='name'>
-              {{ content.name }}
+          <div class='name'>
+            {{ content.name }}
+          </div>
+          <div class='subject'>
+            Ray ka Art
+          </div>
+          <div class='tag-info'>
+            <div class='tag-info-item'>
+              Knowledge tag
             </div>
-            <div class='price'>
-              <span class='price-label'>Price </span>
-              <span class='price-data'>
-                ${{ (content.price || 0).toFixed(2) }}
-              </span>
-              <span class='price-setting'>
-                <a-icon type="edit" :style="{ fontSize: '13px', color: '#999' }"/>
-              </span>
-            </div>
-            <div class='sales'>
-              <span class='sales-label'>Sales </span>
-              <span class='sales-data'>
-                {{ content.sales || 0 }}
-              </span>
+            <div class='tag-info-item'>
+              Commen term
             </div>
           </div>
-          <div class='tag-info'></div>
           <div class='owner'>
-            {{ content.createBy }}
           </div>
         </div>
         <div class='right-info'>
           <div class='update-time'>
-            {{ content.updateTime | dayjs }}
           </div>
         </div>
       </div>
       <div class='action'>
-        <a-space>
-          <a-button type='primary' @click='editItem(content)'>Edit</a-button>
-          <a-button type='primary' @click='handleDeleteItem(content)'>Delete</a-button>
-          <a-button type='primary' @click='handlePublishStatus'>UnPublish</a-button>
-        </a-space>
-      </div>
-    </div>
+        <template v-if='showButton'>
+          <a-space :size='30'>
+            <a-dropdown :trigger="['click']" :getPopupContainer='trigger => trigger.parentElement'>
+              <div class='more-action'>
+                <more-icon />
+              </div>
+              <div class='content-item-more-action' slot='overlay'>
+                <div class='self-learning menu-item' v-if='content.type === typeMap.task'>
+                  Self learning
+                  <a-switch size='small' @change='handleSelfLearning' />
+                </div>
+                <div class='menu-item'>
+                  <custom-button label='Delete' @click='handleDeleteItem'>
+                    <template v-slot:icon>
+                      <delete-icon />
+                    </template>
+                  </custom-button>
+                </div>
+              </div>
+            </a-dropdown>
 
-    <preview-content :preview-current-id='previewCurrentId' :preview-type='previewType' v-if='previewVisible' @close='handlePreviewClose' />
+            <custom-button label='Sub-task' v-if='content.type === typeMap.task && content.subTasks.length > 0'>
+              <template v-slot:icon>
+                <sub-task-icon />
+              </template>
+            </custom-button>
+
+            <custom-button
+              label='Schedule'
+              v-if='content.type === typeMap.task || content.type === typeMap.pd'
+              @click='handleSchedule'>
+              <template v-slot:icon>
+                <schedule-icon />
+              </template>
+            </custom-button>
+
+            <custom-button label='Edit' @click='editItem'>
+              <template v-slot:icon>
+                <edit-icon />
+              </template>
+            </custom-button>
+
+            <custom-button :label="content.status === 0 ? 'Publish' : 'UnPublish'" @click='handlePublishStatus'>
+              <template v-slot:icon>
+                <publish-icon />
+              </template>
+            </custom-button>
+          </a-space>
+        </template>
+      </div>
+
+      <preview-content
+        :preview-current-id='previewCurrentId'
+        :preview-type='previewType'
+        v-if='previewVisible'
+        @close='handlePreviewClose' />
+    </div>
   </div>
 </template>
 
@@ -58,30 +97,60 @@ import * as logger from '@/utils/logger'
 import { DeleteMyContentByType } from '@/api/teacher'
 import { ContentItemMixin } from '@/mixins/ContentItemMixin'
 import PreviewContent from '@/components/MyContentV2/PreviewContent'
+import CustomButton from '@/components/Common/CustomButton'
+import SubTaskIcon from '@/assets/v2/icons/sub-task.svg?inline'
+import EditIcon from '@/assets/v2/icons/edit.svg?inline'
+import PublishIcon from '@/assets/v2/icons/publish_new.svg?inline'
+import ScheduleIcon from '@/assets/v2/icons/schedule.svg?inline'
+import OriginalTipsIcon from '@/assets/v2/icons/original_tips.svg?inline'
+import DeleteIcon from '@/assets/v2/icons/delete.svg?inline'
+import MoreIcon from '@/assets/v2/icons/more.svg?inline'
 
 export default {
-  name: 'PublishedContentItem',
-  components: { PreviewContent },
+  name: 'ContentItem',
+  components: {
+    CustomButton,
+    PreviewContent,
+    SubTaskIcon,
+    EditIcon,
+    PublishIcon,
+    ScheduleIcon,
+    OriginalTipsIcon,
+    DeleteIcon,
+    MoreIcon
+  },
   props: {
     content: {
       type: Object,
       default: null
+    },
+    showButton: {
+      type: Boolean,
+      default: true
+    },
+    clickPreview: {
+      type: Boolean,
+      default: true
     }
   },
-  mixins: [ ContentItemMixin ],
-  data () {
+  mixins: [ContentItemMixin],
+  data() {
     return {
       typeMap: typeMap,
       isSelfLearning: false
     }
   },
+  created() {
+    this.allowPreview = this.clickPreview
+  },
   computed: {
-    status () {
+    status() {
       return this.content.status
     }
   },
   methods: {
-    editItem (item) {
+    editItem() {
+      const item = this.content
       if (item.type === typeMap['unit-plan']) {
         this.$router.push({
           path: '/teacher/unit-plan-redirect/' + item.id
@@ -101,18 +170,24 @@ export default {
       }
     },
 
-    handleSelfLearning (isSelfLearning) {
+    handleSchedule() {
+      this.$router.push({
+        path: '/teacher/schedule-session/' + this.content.id + '/' + this.content.type
+      })
+    },
+
+    handleSelfLearning(isSelfLearning) {
       this.$logger.info('handleSelfLearning', isSelfLearning)
       this.isSelfLearning = isSelfLearning
     },
 
-    handlePublishStatus () {
+    handlePublishStatus() {
       this.$emit('update-publish', {
         content: this.content
       })
     },
 
-    handleDeleteItem () {
+    handleDeleteItem() {
       logger.info('handleDeleteItem', this.content)
       DeleteMyContentByType(this.content).then(res => {
         logger.info('DeleteMyContentByType', res)
@@ -125,7 +200,7 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang='less' scoped>
 @import "~@/components/index.less";
 
 .content-item {
@@ -138,8 +213,8 @@ export default {
   overflow: hidden;
   border-radius: 7px;
   border: 1px solid #EEF1F6;
+
   .cover {
-    border: 1px solid #e1e1e1;
     .cover-block {
       border-radius: 8px;
       height: 9rem;
@@ -198,6 +273,21 @@ export default {
       flex-direction: row;
       align-items: center;
       justify-content: flex-start;
+    }
+  }
+}
+
+.more-action {
+  cursor: pointer;
+  svg {
+    width: 40px;
+    height: 36px;
+    fill: #494B52 !important;
+  }
+
+  &:hover {
+    svg {
+      fill: #14C39A !important;
     }
   }
 }
