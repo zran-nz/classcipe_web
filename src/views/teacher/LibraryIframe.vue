a10916f5f40f6a814173f96e74077029890c632a
yangxunwu <yangxunwu@gmail.com>
2022-05-29 07:15:24 +0800

    添加官网iframe消息处理

--- src/views/teacher/LibraryIframe.vue
+++ src/views/teacher/LibraryIframe.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="library-iframe-container">
-    <iframe v-if='iframeSrc' :src='iframeSrc' class='library-iframe' />
+    <div class='library-search'>
+      <div class='create-new'>
+        <a-space>
+          <global-search-input />
+          <user-profile-avatar />
+        </a-space>
+      </div>
+    </div>
+    <div class='library-iframe-wrapper'>
+      <iframe v-if='iframeSrc' :src='iframeSrc' class='library-iframe' />
+    </div>
   </div>
 </template>
 
 <script>
 
+import GlobalSearchInput from '@/components/GlobalSearch/GlobalSearchInput'
+import UserProfileAvatar from '@/components/User/UserProfileAvatar'
+import { ClasscipeEvent, ClasscipeEventBus } from '@/classcipeEventBus'
 export default {
   name: 'LibraryIframe',
+  components: { UserProfileAvatar, GlobalSearchInput },
   data () {
     return {
       baseUrl: null
@@ -32,23 +46,43 @@ export default {
     } else {
       this.$logger.warn('ContentPreview: unknown host', host)
     }
+
+    ClasscipeEventBus.$on(ClasscipeEvent.LIBRARY_IFRAME_EVENT, this.handleIframeEvent)
+  },
+  beforeDestroy() {
+    ClasscipeEventBus.$off(ClasscipeEvent.LIBRARY_IFRAME_EVENT, this.handleIframeEvent)
   },
   mounted () {
   },
   methods: {
+
+    handleIframeEvent (data) {
+      this.$logger.info('handleIframeEvent', data)
+      return 'ok'
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
 .library-iframe-container {
+  background-color: #fff;
   height: 100vh;
   width: 100%;
-  .library-iframe {
-    height: 100vh;
+  .library-search {
+    height: 80px;
     width: 100%;
-    border: none;
-    outline: none;
+  }
+
+  .library-iframe-wrapper {
+    height: calc(100vh - 80px);
+    width: 100%;
+    .library-iframe {
+      height: 100%;
+      width: 100%;
+      border: none;
+      outline: none;
+    }
   }
 }
 </style>
