d33b48ed469126c935785e7bf8ee85161f33781f
yangxunwu <yangxunwu@gmail.com>
2022-06-01 08:18:27 +0800

    fix: eslint warning

--- src/components/PdContent/CaseVideo.vue
+++ src/components/PdContent/CaseVideo.vue
@@ -17,6 +17,9 @@
 import ClasscipeDriveButton from '@/components/ClasscipeDrive/ClasscipeDriveButton'
 import ScreenCapture from '@/components/ScreenCapture/ScreenCapture'
 import VideoList from '@/components/PdContent/VideoList'
+import ClasscipeDriveEvent from '@/components/ClasscipeDrive/ClasscipeDriveEvent'
+import VideoEvent from '@/components/Video/VideoEvent'
+import DriveType from '@/components/ClasscipeDrive/Content/DriveType'
 
 export default {
   name: 'CaseVideo',
@@ -26,10 +29,40 @@ export default {
     }
   },
   created() {
+    this.$EventBus.$on(ClasscipeDriveEvent.INSERT_DRIVE_ITEM, this.handleSelectDriveItem)
+    this.$EventBus.$on(ClasscipeDriveEvent.INSERT_YOUTUBE_ITEM, this.handleSelectYoutube)
+    this.$EventBus.$on(ClasscipeDriveEvent.INSERT_GOOGLE_DRIVE, this.handleSelectGoogleDrive)
+    this.$EventBus.$on(VideoEvent.VIDEO_ADD, this.handleAddScreenCapture)
   },
   methods: {
     handleCapturedVideoData(data) {
 
+    },
+
+    handleSelectDriveItem (driveItem) {
+      this.$logger.info('handleSelectDriveItem', driveItem[[0]])
+      this.currentMediaFileUrl = driveItem[0].filePath
+      this.currentDriveType = DriveType.ClasscipeDrive
+      this.afterSelectInsert()
+    },
+    handleSelectYoutube (youtubeItem) {
+      this.$logger.info('handleSelectYoutube', youtubeItem)
+      this.currentMediaFileUrl = youtubeItem.link
+      this.currentDriveType = DriveType.Youtube
+      this.afterSelectInsert()
+    },
+    handleSelectGoogleDrive (googleDriveItem) {
+      this.$logger.info('handleSelectGoogleDrive', googleDriveItem)
+      this.currentMediaFileUrl = googleDriveItem.url
+      this.currentDriveType = DriveType.GoogleDrive
+      this.afterSelectInsert()
+    },
+
+    handleAddScreenCapture (url) {
+      this.$logger.info('handleAddScreenCapture', url)
+      this.currentMediaFileUrl = url
+      this.currentDriveType = DriveType.Upload
+      this.afterSelectInsert()
     }
   }
 }
