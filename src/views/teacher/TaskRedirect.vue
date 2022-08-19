4b4aaa0a952b60d79b902caec7ac26eaaf69e63f
yangxunwu <yangxunwu@gmail.com>
2022-08-19 22:03:20 +0800

    update

--- src/views/teacher/TaskRedirect.vue
+++ src/views/teacher/TaskRedirect.vue
@@ -1,19 +1,56 @@
 <template>
   <a-card :bordered="false" :bodyStyle="{ padding: '16px 24px', height: '100%', minHeight: '500px' }">
     <a-spin class="redirect-spin" tip="Loading..." size="large"/>
+    <a-modal
+      v-model='visible'
+      destroyOnClose
+      :title='null'
+      @cancel='$router.go(-1)'
+      @ok='select(curriculumId)'
+      :closable='false'>
+      <modal-header title='Choose a curriculum' :allow-close='false'></modal-header>
+      <a-radio-group v-model="curriculumId">
+        <a-radio :style="radioStyle" v-for='curriculum in list' :value='curriculum.curriculumId' :key='curriculum.curriculumId'>
+          {{ curriculum.curriculumName }}
+        </a-radio>
+      </a-radio-group>
+    </a-modal>
   </a-card>
 </template>
 
 <script>
 import { TaskAddOrUpdate } from '@/api/task'
 import { ClasscipeEventBus, ClasscipeEvent } from '@/classcipeEventBus'
+import { getCurriculumBySchoolId } from '@/api/academicSettingCurriculum'
+import ModalHeader from '@/components/Common/ModalHeader'
+import { mapState } from 'vuex'
 export default {
   name: 'TaskRedirect',
+  components: {
+    ModalHeader
+  },
   props: {
     taskId: {
       type: String,
       default: null
     }
+  },
+  data () {
+    return {
+      visible: false,
+      curriculumId: null,
+      list: [],
+      radioStyle: {
+        display: 'block',
+        height: '30px',
+        lineHeight: '30px'
+      }
+    }
+  },
+  computed: {
+    ...mapState({
+      currentSchool: state => state.user.currentSchool
+    })
   },
   created () {
     this.$logger.info('task redirecting ' + this.taskId)
@@ -29,24 +66,56 @@ export default {
       if (this.taskId) {
         this.$router.replace('/teacher/add-task-v2/' + this.taskId)
       } else {
-        const data = {
-          name: 'Untitled task',
-          status: 0
-        }
-
-        TaskAddOrUpdate(data).then((response) => {
-          this.$logger.info('TaskAddOrUpdate response', response.result)
-          if (response.success) {
-            if (response.code !== 520 && response.code !== 403) {
-              this.$router.replace('/teacher/add-task-v2/' + response.result.id)
+        getCurriculumBySchoolId({
+          schoolId: this.currentSchool.id
+        }).then(res => {
+          if (res.code === 0) {
+            if (res.result && res.result.length > 0) {
+              console.log('getCurriculumBySchoolId', res.result)
+              this.list = res.result
+              this.curriculumId = this.list[0].curriculumId
+              if (this.list.length > 1) {
+                this.visible = true
+              } else {
+                this.select(this.curriculumId)
+              }
             } else {
-              this.$logger.info('等待授权回调')
+              this.$confirm({
+                title: 'Alert',
+                okText: 'Confirm',
+                cancelText: 'Cancel',
+                content: 'You have not bound the curriculum.',
+                onOk: () => {
+                  this.$router.go(-1)
+                },
+                onCancel() {
+                  this.$router.go(-1)
+                }
+              })
             }
-          } else {
-            this.$message.error(response.message)
           }
         })
       }
+    },
+    select(curriculumId) {
+      const data = {
+        name: 'Untitled task',
+        status: 0,
+        curriculumId: curriculumId
+      }
+
+      TaskAddOrUpdate(data).then((response) => {
+        this.$logger.info('TaskAddOrUpdate response', response.result)
+        if (response.success) {
+          if (response.code !== 520 && response.code !== 403) {
+            this.$router.replace('/teacher/add-task-v2/' + response.result.id)
+          } else {
+            this.$logger.info('等待授权回调')
+          }
+        } else {
+          this.$message.error(response.message)
+        }
+      })
     }
   }
 }
