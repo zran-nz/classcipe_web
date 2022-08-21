3ad000ee7e0a116ac4c217212ce0227fd5cfcc30
yangxunwu <yangxunwu@gmail.com>
2022-08-21 15:15:44 +0800

    update

--- src/views/teacher/LinkedContentList.vue
+++ src/views/teacher/LinkedContentList.vue
@@ -2,40 +2,37 @@
   <div class='my-sub-task-list'>
     <fixed-vertical-header>
       <template v-slot:left>
-        <a-button class='cc-dark-button' @click='handleCreateSubtask'>
-          {{ displayLabel }} linked content
-        </a-button>
-      </template>
-      <template v-slot:right>
-        <a-space v-show='selectedTaskList.length > 0'>
-          <a-button class='cc-dark-button' @click='publishSelected'>
-            Publish
+        <a-tooltip placement='leftBottom'>
+          <template slot='title'>
+            The task is more likely to be purchased by other educators if it is simple, clear and focus on specific learning outcome(s).
+            So dividing your main task into small sub-tasks with specific tags is always a good idea.
+          </template>
+          <a-button class='cc-dark-button' @click='handleCreateSubtask'>
+            Create sub task
           </a-button>
-          <a-button class='cc-dark-button' @click='unPublishSelected'>
-            Unpublish
-          </a-button>
-        </a-space>
+        </a-tooltip>
       </template>
     </fixed-vertical-header>
     <div class='sub-task-container'>
-      <div class='sub-task-list vertical-left' v-for='content in linkedContentList' :key='content.id'>
-        <div class='checked-icon vertical-center' @click='toggleSelectItem(content)'>
-          <a-checkbox :checked='selectedTaskList.indexOf(content) !== -1' :disabled='content.needComplete'></a-checkbox>
-        </div>
+      <div class='sub-task-list vertical-left' v-for='content in subTaskList' :key='content.id'>
         <div class='task-item vertical-left'>
-          <div class='complete-mask' v-if='content.needComplete' @click='handleEditItem(content)'>
-            <a-button type='primary' shape='round' class='cc-complete-button' size='large'>Complete</a-button>
-          </div>
           <content-item
+            @delete='loadAssociateData'
             :content='content'
             :show-button='true'
             :show-edit='true'
             :show-delete='true'
             :show-schedule='true'
-            :click-preview='false'/>
+            :show-publish='true'
+            :show-set-price='content.status === 1'
+            @update-publish='handleShowContentPublish'
+            :show-publish-status='false'/>
         </div>
       </div>
     </div>
+
+    <verification-tip ref="verificationTip" @continue="doUpdatePublish"/>
+    <edit-price-dialog :content='currentContent' ref='editPrice' @finish='showPublishTips' v-if='currentContent'/>
   </div>
 </template>
 
@@ -43,16 +40,19 @@
 import FixedFormHeader from '@/components/Common/FixedFormHeader'
 import FixedVerticalHeader from '@/components/Common/FixedVerticalHeader'
 import ContentItem from '@/components/MyContentV2/ContentItem'
-import { GetAssociate } from '@/api/teacher'
-import { getLabelNameType, typeMap } from '@/const/teacher'
-import { PlanField, TaskField } from '@/const/common'
-import storage from 'store'
-import { ACCESS_TOKEN } from '@/store/mutation-types'
-import CustomTextButton from '@/components/Common/CustomTextButton'
+import { TaskField } from '@/const/common'
+import FixedFormFooter from '@/components/Common/FixedFormFooter'
+import { getCookie } from '@/utils/util'
+import { TEACHER_SECURITY_NOT_SHOW } from '@/store/mutation-types'
+import VerificationTip from '@/components/MyContentV2/VerificationTip'
+import { GetAssociate, UpdateContentStatus } from '@/api/teacher'
+import { GoogleAuthCallBackMixin } from '@/mixins/GoogleAuthCallBackMixin'
+import { typeMap } from '@/const/teacher'
+import EditPriceDialog from '@/components/MyContentV2/EditPriceDialog'
 
 export default {
   name: 'LinkedContentList',
-  components: { CustomTextButton, ContentItem, FixedVerticalHeader, FixedFormHeader },
+  components: { EditPriceDialog, VerificationTip, FixedFormFooter, ContentItem, FixedVerticalHeader, FixedFormHeader },
   props: {
     contentId: {
       type: String,
@@ -63,46 +63,27 @@ export default {
       required: true
     }
   },
+  mixins: [GoogleAuthCallBackMixin],
   data() {
     return {
       loading: false,
-      typeMap: typeMap,
-      linkedContentList: [],
+      publishLoading: false,
+      subTaskList: [],
       selectedTaskList: [],
-      requiredPlanFields: [
-        PlanField.Name,
-        PlanField.Image,
-        PlanField.Inquiry,
-        PlanField.Scenarios,
-        PlanField.Question,
-        PlanField.LearnOuts
-      ],
       requiredTaskFields: [
         TaskField.Name,
         TaskField.Image,
         TaskField.Overview,
         TaskField.LearnOuts
-      ]
+      ],
+      typeMap: typeMap,
+      currentContent: null
     }
   },
-  computed: {
-    displayLabel () {
-      return getLabelNameType(parseInt(this.contentType))
-    }
-  },
-  async created() {
-    const token = storage.get(ACCESS_TOKEN)
-    this.loading = true
-    await this.$store.dispatch('loadFormConfigData', token)
+  created() {
     this.loadAssociateData()
   },
   methods: {
-    handleCreateSubtask () {
-      // this.$router.push({
-      //   path: '/teacher/split-task/' + this.taskId
-      // })
-    },
-
     async loadAssociateData () {
       try {
         const slideData = await GetAssociate({
@@ -112,7 +93,7 @@ export default {
         })
         console.log('loadAssociateData', slideData)
         const list = slideData.result.owner
-        this.linkedContentList = []
+        this.subTaskList = []
         const taskList = []
         const unitList = []
         list.forEach(item => {
@@ -127,11 +108,9 @@ export default {
           })
         })
         console.log('taskList', taskList)
-        this.checkTaskAllowPublished(taskList)
         console.log('unitList', unitList)
-        this.checkUnitAllowPublished(unitList)
-        this.linkedContentList = [...unitList, ...taskList]
-        this.$logger.info('sub list', this.linkedContentList)
+        this.subTaskList = [...taskList]
+        this.$logger.info('sub list', this.subTaskList)
       } catch (e) {
         console.error('loadSlideData', e)
       } finally {
@@ -139,81 +118,77 @@ export default {
       }
     },
 
-    // 检查必填项是否为空,只检测null,undefined,空字符串,空数组,空对象
-    isEmpty(value) {
-       if (value === null || value === '' || value === undefined) {
-         return true
-       }
-       if (value.hasOwnProperty('length') && value.length === 0) {
-         return true
-       }
-
-       return JSON.stringify(value) === '{}'
-     },
-
-    checkTaskAllowPublished (taskList) {
-      taskList.forEach(item => {
-        item.needComplete = false
-        this.requiredTaskFields.forEach(field => {
-          if (this.isEmpty(item[field])) {
-            item.needComplete = true
-            this.$logger.info('task need complete', item)
-          }
-        })
+    handleCreateSubtask () {
+      this.$router.push({
+        path: '/teacher/split-task/' + this.taskId
       })
     },
-    checkUnitAllowPublished (unitList) {
-      unitList.forEach(item => {
-        item.needComplete = false
-        this.requiredPlanFields.forEach(field => {
-          if (this.isEmpty(item[field])) {
-            item.needComplete = true
-            this.$logger.info('plan need complete', item)
-          }
-        })
-      })
+    handleShowContentPublish(data) {
+      this.$logger.info('handleShowContentPublish', data)
+      this.currentContent = data.content
+      if ((this.currentContent.type === this.typeMap.task ||
+        this.currentContent.type === this.typeMap.pd)) {
+        if (!this.currentContent.presentationId || this.currentContent.presentationId.startsWith('fake_')) {
+          this.$message.warn('This task/PD content can not be published without interactive slides, please edit google slides first')
+          return
+        }
+      }
+      this.handleUpdatePublish(data)
     },
 
-    toggleSelectItem (item) {
-      this.$logger.info('toggleSelectItem', item)
-      const index = this.selectedTaskList.findIndex(subTask => subTask.id === item.id)
-      this.$logger.info('index', index)
-      if (index === -1) {
-        this.selectedTaskList.push(item)
-        this.$logger.info('push item', this.selectedTaskList)
+    handleUpdatePublish(data) {
+      const targetStatus = data.status ? 0 : 1
+      // 发布的时候需要判断是否有老师认证
+      // 没有老师认证的情况下，询问是否需要进行老师认证
+      // 勾选不再进行询问时，则不再跳出
+      if (targetStatus) {
+        const isNotShowSecurity = getCookie(TEACHER_SECURITY_NOT_SHOW)
+        if (!isNotShowSecurity) {
+          // TODO 查询是否已经进行老师认证
+          const isExists = false
+          if (!isExists) {
+            this.$refs.verificationTip.doCreate()
+            return
+          }
+        }
+      }
+      this.doUpdatePublish(data)
+    },
+
+    doUpdatePublish (data) {
+      data = data || this.currentContent
+      this.$logger.info('handleUpdatePublish', data)
+      const index = this.subTaskList.findIndex(item => item.id === data.id)
+      if (index !== -1) {
+        const targetStatus = data.status ? 0 : 1
+        this.publishLoading = true
+        UpdateContentStatus({
+          id: data.id,
+          status: targetStatus,
+          type: data.type
+        }).then((res) => {
+          if (res.code === 520 || res.code === 403) {
+            this.$logger.info('等待授权回调')
+            this.$message.loading('Waiting for Google Slides auth...', 10)
+            return
+          }
+          this.$logger.info('handlePublishStatus res', res, 'currentContent', this.currentContent)
+          this.subTaskList[index].status = targetStatus
+          if (targetStatus) {
+            this.$refs.editPrice.showEditPrice()
+          } else {
+            this.$message.success('Unpublish successfully!')
+          }
+        }).finally(() => {
+          this.publishLoading = false
+        })
       } else {
-        this.selectedTaskList.splice(index, 1)
-        this.$logger.info('delete item', index)
+        this.$logger.warn(`no found Update item ${data.id}`)
+        this.currentContent = null
       }
-      this.$logger.info('selected list', this.selectedTaskList)
     },
-
-    publishSelected () {
-      this.$logger.info('publishSelected', this.selectedTaskList)
-    },
-
-    unPublishSelected () {
-      this.$logger.info('unPublishSelected', this.selectedTaskList)
-    },
-
-    handleEditItem (item) {
-      if (item.type === typeMap['unit-plan']) {
-        this.$router.push({
-          path: '/teacher/unit-plan-redirect/' + item.id
-        })
-      } else if (item.type === typeMap.task) {
-        this.$router.push({
-          path: '/teacher/task-redirect/' + item.id
-        })
-      } else if (item.type === typeMap.video) {
-        this.$router.push({
-          path: '/teacher/video-redirect/' + item.id
-        })
-      } else if (item.type === typeMap.pd) {
-        this.$router.push({
-          path: '/teacher/pd-content-redirect/' + item.id
-        })
-      }
+    showPublishTips () {
+      this.$message.success('Publish successfully!')
     }
   }
 }
@@ -228,31 +203,40 @@ export default {
   padding: 10px 20px;
   overflow-y: auto;
   .sub-task-list {
+    position: relative;
     background: #fff;
     padding: 0 15px;
+    margin-bottom: 15px;
     .checked-icon {
+      width: 33px;
       padding-right: 15px;
+
+      .ant-checkbox {
+        border: 2px solid #15C39A;
+      }
+    }
+
+    .action-mask {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      cursor: pointer;
+      background-color: rgba(0, 0, 0, 0.5);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #fff;
+      font-size: 16px;
+      user-select: none;
+      border-radius: 5px;
+      z-index: 1100;
     }
   }
 }
 
 .task-item {
   width: 100%;
-  position: relative;
-
-  .complete-mask {
-    position: absolute;
-    left: 0;
-    top: 10px;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    width: 100%;
-    height: calc(100% - 20px);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 100;
-    border-radius: 5px;
-  }
 }
 </style>
