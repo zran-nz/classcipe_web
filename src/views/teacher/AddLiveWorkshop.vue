7298c19e97eb37751b68893ea666b0656bafd70e
jacob <jacob.sun@i-click.com>
2022-08-29 13:51:50 +0800

    fix bug

--- src/views/teacher/AddLiveWorkshop.vue
+++ src/views/teacher/AddLiveWorkshop.vue
@@ -600,7 +600,12 @@ export default {
     },
 
     handleBack () {
-      this.$router.go(-1)
+      if (window.history.length <= 1) {
+        this.$router.push({ path: '/teacher/main/live-workshops' })
+        return false
+      } else {
+        this.$router.go(-1)
+      }
     },
 
     getSessionStep() {
--- src/views/teacher/schedule/ScheduleWorkshop.vue
+++ src/views/teacher/schedule/ScheduleWorkshop.vue
@@ -191,7 +191,12 @@ export default {
     },
 
     handleBack () {
-      this.$router.go(-1)
+      if (window.history.length <= 1) {
+        this.$router.push({ path: '/teacher/main/live-workshops' })
+        return false
+      } else {
+        this.$router.go(-1)
+      }
     },
 
     handleSelectUnit(data) {
