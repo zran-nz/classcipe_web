fa806f618363d2f9c297b3523c6c4d7aa4451fc3
Acan <abans.cn@gmail.com>
2022-09-12 22:24:48 +0000

    up logout

--- src/views/account/logout.vue
+++ src/views/account/logout.vue
@@ -2,13 +2,11 @@
 
 </template>
 <script>
-import storage from 'store'
-import { ACCESS_TOKEN } from '@/store/mutation-types'
 export default {
   async mounted() {
-    const token =  storage.get(ACCESS_TOKEN)
-    const rs = await fetch('/classcipe/sys/logout', { headers: {'x-access-token': token } }).then(r => r.json())
-    this.$router.replace('/user/login')
+    this.$store.dispatch('Logout').then(() => {
+      this.$router.push({ name: 'login' })
+    })
   }
 }
 </script>
