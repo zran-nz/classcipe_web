615dd99f3a5a146a71e7439a0c7855835a5b9772
Acan <abans.cn@gmail.com>
2022-09-19 09:57:46 +0000

    up iframe

--- src/views/teacher/AddTaskV2.vue
+++ src/views/teacher/AddTaskV2.vue
@@ -179,7 +179,7 @@
                       :placeholder='taskLabelHint(taskField.Question, $store.getters.formConfigData) || "Search key question(s)"' />
                   </custom-form-item>
                 </div>
-                <div v-else-if='fieldItem.fieldName === taskField.LearnOuts' class='form-block tag-content-block' style="height:100%;">
+                <div v-else-if='fieldItem.fieldName === taskField.LearnOuts' class='form-block tag-content-block' style="height:100%;margin:-20px -30px;">
                   <iframe style="width:100%;height:100%;" :src="`/v2/com/task/outline/${taskId}?token=${token}`" frameborder="0"></iframe>
                   <!-- <div class='is-self-learning vertical-between'>
                     <div class='self-learning-label'>
