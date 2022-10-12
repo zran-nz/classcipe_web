99cd88e1188d874f6702e9d1575e464647162ff7
Acan <abans.cn@gmail.com>
2022-10-12 01:18:26 +0000

    up worksho

--- src/api/v2/calendarSchedule.js
+++ src/api/v2/calendarSchedule.js
@@ -18,28 +18,27 @@ export async function QueryForCalendar (parameter) {
     params: parameter
   })
   // Combine new register data
-  if (parameter.workshopStatus.includes('4')) { // load register data
+  if (parameter?.workshopStatus?.includes('4')) { // load register data
     // console.log(parameter, rs.result[0], 111111111)
     const start = new Date(new Date(parameter.startDate).getTime()+86400000).toLocaleDateString()
     const end = new Date(new Date(parameter.endDate).getTime()-86400000).toLocaleDateString()
-    const list = await App.service('register').get('oldList', { query: { zone: new Date().getTimezoneOffset(), start, end, school: parameter.schoolIds.split(',') }})
+    const list = await App.service('session').get('dateList', { query: { zone: new Date().getTimezoneOffset(), start, end, school: parameter.schoolIds.split(',') }})
     for (const v of list) {
       v.sessionId = v.sid
       v.schoolId = v.school || '0'
       v.startTime = v.start
-      v.startTime = v.start
       v.endTime = v.end
-      v.userAvatar = v.avatar
-      v.userRealName = v.nickname
-      v.workshopsStatus = v.status === -1 ? 2 : 1
-      v.title = v.data?.name
-      v.cover = v.data?.image
+      v.userAvatar = v.owner.avatar
+      v.userRealName = v.owner.nickname
+      v.workshopsStatus = v.status === 'close' ? 2 : 1
+      v.title = v.name
+      v.cover = v.image
       v.content = {}
-      v.sessionInfo = { id: v.data?.id || v.sid, type: 'workshop', isLib: true }
+      v.sessionInfo = { id: v.sid, type: 'workshop', isLib: true }
       v.unitPlanInfo = null
       v.workshopsDetailInfo = {
         sessionId: v.sid, sessionStartTime: v.start, sessionEndTime: v.end,
-        userAvatar: v.avatar, userRealName: v.nickname, title: v.data?.name
+        userAvatar: v.owner.avatar, userRealName: v.owner.nickname, title: v.name
       }
     }
     rs.result.push(...list)
